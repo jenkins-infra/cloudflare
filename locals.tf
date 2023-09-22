328b1a1fd81f837f15558c27e94efa0952715f08
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2023-09-22 18:37:37 +0200

    feat(updates.jenkins.io): create a R2 bucket in Western Europe (#2)
    * feat(updates.jenkins.io): create a R2 bucket in Western Europe
    
    * Update locals.tf
    
    * Update locals.tf
    
    ---------
    
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

---
+++ locals.tf
@@ -0,0 +1,5 @@
+locals {
+  account_id = {
+    jenkins-infra-team = "8d1838a43923148c5cee18ccc356a594"
+  }
+}
---
+++ updates.jenkins.io.tf
@@ -0,0 +1,5 @@
+resource "cloudflare_r2_bucket" "westeurope_updates_jenkins_io" {
+  account_id = local.account_id["jenkins-infra-team"]
+  name       = "westeurope-updates-jenkins-io"
+  location   = "weur"
+}
