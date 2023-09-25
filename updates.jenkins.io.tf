ab7df48226ddb886c7360823ccc854a6788a4e42
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2023-09-25 14:40:31 +0200

    feat(updates.jenkins.io): add `westeurope.cloudflare.jenkins.io` zone (#9)
    This PR create the `westeurope.cloudflare.jenkins.io` zone which will be
    used as custom public domain for the corresponding R2 bucket already
    defined here:
    https://github.com/jenkins-infra/cloudflare/blob/688cfff72c164948207f83151ee4cd0338a1f1b0/updates.jenkins.io.tf#L1-L5
    
    Note: this custom domain link will be made later via CloudFlare admin,
    not possible neither from the terraform provider neither the API at the
    moment, cf
    https://github.com/cloudflare/terraform-provider-cloudflare/issues/2537
    
    Ref: https://github.com/jenkins-infra/helpdesk/issues/2649

--- updates.jenkins.io.tf
+++ updates.jenkins.io.tf
@@ -1,3 +1,27 @@
+# Keeping this main zone imported as data only so we don't risk destroying it (the sponsoring is attached to it)
+data "cloudflare_zone" "cloudflare_jenkins_io" {
+  account_id = local.account_id["jenkins-infra-team"]
+  name       = "cloudflare.jenkins.io"
+}
+
+## West Europe
+resource "cloudflare_zone" "westeurope_cloudflare_jenkins_io" {
+  account_id = local.account_id["jenkins-infra-team"]
+  zone       = "westeurope.cloudflare.jenkins.io"
+}
+
+resource "cloudflare_record" "ns_westeurope" {
+  count = 2
+
+  zone_id = data.cloudflare_zone.cloudflare_jenkins_io.id
+  name    = "westeurope"
+  value   = cloudflare_zone.westeurope_cloudflare_jenkins_io.name_servers[count.index]
+  type    = "NS"
+  ttl     = 60
+
+  tags = local.default_tags
+}
+
 resource "cloudflare_r2_bucket" "westeurope_updates_jenkins_io" {
   account_id = local.account_id["jenkins-infra-team"]
   name       = "westeurope-updates-jenkins-io"
