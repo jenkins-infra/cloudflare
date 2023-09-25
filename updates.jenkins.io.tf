b9e66f266aabbc87463a39a30d7060b3b0520825
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2023-09-25 09:25:15 +0200

    apply review suggestions

--- updates.jenkins.io.tf
+++ updates.jenkins.io.tf
@@ -1,9 +1,3 @@
-resource "cloudflare_r2_bucket" "westeurope_updates_jenkins_io" {
-  account_id = local.account_id["jenkins-infra-team"]
-  name       = "westeurope-updates-jenkins-io"
-  location   = "WEUR"
-}
-
 # Keeping this main zone imported as data only so we don't risk destroying it (the sponsoring is attached to it)
 data "cloudflare_zone" "cloudflare_jenkins_io" {
   account_id = local.account_id["jenkins-infra-team"]
@@ -11,8 +5,13 @@ data "cloudflare_zone" "cloudflare_jenkins_io" {
 }
 
 ## West Europe
-resource "cloudflare_record" "westeurope" {
-  for_each = toset(data.cloudflare_zone.cloudflare_jenkins_io.name_servers)
+resource "cloudflare_zone" "westeurope_cloudflare_jenkins_io" {
+  account_id = local.account_id["jenkins-infra-team"]
+  zone       = "westeurope.cloudflare.jenkins.io"
+}
+
+resource "cloudflare_record" "ns_westeurope" {
+  for_each = toset(cloudflare_zone.westeurope_cloudflare_jenkins_io.name_servers)
 
   zone_id = data.cloudflare_zone.cloudflare_jenkins_io.id
   name    = "westeurope"
@@ -21,10 +20,8 @@ resource "cloudflare_record" "westeurope" {
   ttl     = 60
 }
 
-resource "cloudflare_zone" "westeurope_cloudflare_jenkins_io" {
+resource "cloudflare_r2_bucket" "westeurope_updates_jenkins_io" {
   account_id = local.account_id["jenkins-infra-team"]
-  zone       = "westeurope.cloudflare.jenkins.io"
-  depends_on = [
-    cloudflare_record.westeurope
-  ]
+  name       = "westeurope-updates-jenkins-io"
+  location   = "WEUR"
 }
