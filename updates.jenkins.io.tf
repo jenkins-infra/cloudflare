1fdff8e62fb1a96cb73fccf7aaaba953b258a0b0
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2023-09-26 16:13:51 +0200

    chore(updates.jenkins.io): comment out `westeurope` zone to recreate it later

--- updates.jenkins.io.tf
+++ updates.jenkins.io.tf
@@ -1,8 +1,8 @@
 # West Europe
-resource "cloudflare_zone" "westeurope_cloudflare_jenkins_io" {
-  account_id = local.account_id["jenkins-infra-team"]
-  zone       = "westeurope.cloudflare.jenkins.io"
-}
+# resource "cloudflare_zone" "westeurope_cloudflare_jenkins_io" {
+#   account_id = local.account_id["jenkins-infra-team"]
+#   zone       = "westeurope.cloudflare.jenkins.io"
+# }
 
 resource "cloudflare_r2_bucket" "westeurope_updates_jenkins_io" {
   account_id = local.account_id["jenkins-infra-team"]
@@ -10,6 +10,6 @@ resource "cloudflare_r2_bucket" "westeurope_updates_jenkins_io" {
   location   = "WEUR"
 }
 
-output "westeurope_cloudflare_jenkins_io_ns_records" {
-  value = cloudflare_zone.westeurope_cloudflare_jenkins_io.name_servers
-}
+# output "westeurope_cloudflare_jenkins_io_ns_records" {
+#   value = cloudflare_zone.westeurope_cloudflare_jenkins_io.name_servers
+# }
