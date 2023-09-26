594f456806bf579570cbdccd7673e5d1d701f84b
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2023-09-26 16:41:06 +0200

    try again

--- updates.jenkins.io.tf
+++ updates.jenkins.io.tf
@@ -14,7 +14,5 @@ resource "cloudflare_r2_bucket" "updates_jenkins_io" {
 }
 
 output "zones_ns_records" {
-  for_each = local.regions
-
-  value = cloudflare_zone.updates_jenkins_io.name_servers[each.key]
+  value = cloudflare_zone.updates_jenkins_io[*].name_servers
 }
