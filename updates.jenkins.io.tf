56a5ba2e0451e943ecf2b2021d592dc56e062d4c
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2023-09-28 20:14:07 +0200

    chore: comment out to recreate later

--- updates.jenkins.io.tf
+++ updates.jenkins.io.tf
@@ -1,20 +1,20 @@
-resource "cloudflare_zone" "updates_jenkins_io" {
-  for_each = local.regions
+# resource "cloudflare_zone" "updates_jenkins_io" {
+#   for_each = local.regions
 
-  account_id = local.account_id["jenkins-infra-team"]
-  zone       = "${each.key}.cloudflare.jenkins.io"
-}
+#   account_id = local.account_id["jenkins-infra-team"]
+#   zone       = "${each.key}.cloudflare.jenkins.io"
+# }
 
-resource "cloudflare_r2_bucket" "updates_jenkins_io" {
-  for_each = local.regions
+# resource "cloudflare_r2_bucket" "updates_jenkins_io" {
+#   for_each = local.regions
 
-  account_id = local.account_id["jenkins-infra-team"]
-  name       = "${each.key}-updates-jenkins-io"
-  location   = "${each.value}"
-}
+#   account_id = local.account_id["jenkins-infra-team"]
+#   name       = "${each.key}-updates-jenkins-io"
+#   location   = "${each.value}"
+# }
 
-output "zones_name_servers" {
-  value = {
-    for k, zone in cloudflare_zone.updates_jenkins_io : k => zone.name_servers
-  }
-}
+# output "zones_name_servers" {
+#   value = {
+#     for k, zone in cloudflare_zone.updates_jenkins_io : k => zone.name_servers
+#   }
+# }
