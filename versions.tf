947b3f6dc18660c8893528d19d265e8be0d0bb85
Damien Duportal <damien.duportal@gmail.com>
2025-03-19 18:55:38 +0100

    chore: bump cloudflare provider to '4.52.0'
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- versions.tf
+++ versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = ">= 1.11, <1.12"
   required_providers {
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
+      version = "~> 4"
     }
   }
 }
