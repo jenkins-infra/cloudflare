77d6350189b8823c06aa0bd731765655f30beb18
jenkins-infra-updatecli <178728+jenkins-infra-updatecli[bot]@users.noreply.github.com>
2025-11-26 01:19:29 +0000

    chore: Update expiration date for the updates.jenkins.io R2 buckets
    Made with ❤️️ by updatecli

--- locals.tf
+++ locals.tf
@@ -8,7 +8,7 @@ locals {
     eastamerica = "ENAM"
   }
 
-  r2_token_expiration_date = "2026-02-23T00:00:00Z"
+  r2_token_expiration_date = "2026-02-24T00:00:00Z"
 
   r2_api_permissions = {
     for x in data.cloudflare_account_api_token_permission_groups_list.this.result :
